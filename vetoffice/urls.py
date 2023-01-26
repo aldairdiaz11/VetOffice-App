01aafe49384596b642a56f3d27b288298a0a5452
aldairdiaz <irving.s21diaz@gmail.com>
2023-01-26 10:29:05 -0600

    "Set urlpatterns"

--- vetoffice/urls.py
+++ vetoffice/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name='home')
+    path("", views.home, name='home'),
+    path("owner/list", views.OwnerList.as_view(), name="ownerlist"),
+    path("patient/list", views.PatientList.as_view(), name="patientlist"),
+    path("owner/create", views.OwnerCreate.as_view(), name="ownercreate"),
+    path("patient/create", views.PatientCreate.as_view(), name="patientcreate")
 ]
